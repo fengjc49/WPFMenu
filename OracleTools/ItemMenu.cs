de2f312056076da47ca3ee1965bdbe5552e8d2d8
fengjc49 <fengjc49@163.com>
2020-02-26 15:06:42 +0800

    创建资料

---
+++ OracleTools/ItemMenu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using MaterialDesignThemes.Wpf;
+
+namespace OracleTools
+{
+    public class ItemMenu
+    {
+        public ItemMenu(string header, List<SubItem> subItems, PackIconKind icon)
+        {
+            Header = header;
+            SubItems = subItems;
+            Icon = icon;
+        }
+
+        public string Header { get; private set; }
+        public PackIconKind Icon { get; private set; }
+        public List<SubItem> SubItems { get; private set; }
+        public UserControl Screen { get; private set; }
+    }
+}
---
+++ OracleTools/SubItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace OracleTools
+{
+    public class SubItem
+    {
+        public SubItem(string name, UserControl screen = null)
+        {
+            Name = name;
+            Screen = screen;
+        }
+
+        public string Name { get; private set; }
+        public UserControl Screen { get; private set; }
+    }
+}
